43489f6c807c475c5dc25723cf7dc29d017c28d0
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2022-05-27 18:33:01 +0800

    add start pos to ret

--- luabinding.c
+++ luabinding.c
@@ -1,22 +1,21 @@
-#include "lua.h"
-#include "lualib.h"
-#include "lauxlib.h"
-#include <string.h>
-#include <stdlib.h>
 #include <limits.h>
+#include <stdlib.h>
+#include <string.h>
 #include "fibheap.h"
 #include "jps.h"
+#include "lauxlib.h"
+#include "lua.h"
+#include "lualib.h"
 
 #ifdef __PRINT_DEBUG__
-#define deep_print(format,...) printf(format, ##__VA_ARGS__)
+#define deep_print(format, ...) printf(format, ##__VA_ARGS__)
 #else
-#define deep_print(format,...)
+#define deep_print(format, ...)
 #endif
 
 #define MT_NAME ("_jps_search_metatable")
 
-static inline int
-getfield(lua_State *L, const char *f) {
+static inline int getfield(lua_State* L, const char* f) {
     if (lua_getfield(L, -1, f) != LUA_TNUMBER) {
         return luaL_error(L, "invalid type %s", f);
     }
@@ -25,8 +24,7 @@ getfield(lua_State *L, const char *f) {
     return v;
 }
 
-static inline int
-setobstacle(lua_State *L, struct map *m, int x, int y) {
+static inline int setobstacle(lua_State* L, struct map* m, int x, int y) {
     if (!check_in_map(x, y, m->width, m->height)) {
         luaL_error(L, "Position (%d,%d) is out of map", x, y);
     }
@@ -34,8 +32,7 @@ setobstacle(lua_State *L, struct map *m, int x, int y) {
     return 0;
 }
 
-
-static inline void push_table_to_stack(lua_State *L, int x, int y, int num) {
+static inline void push_table_to_stack(lua_State* L, int x, int y, int num) {
     lua_newtable(L);
     lua_pushinteger(L, x);
     lua_rawseti(L, -2, 1);
@@ -44,8 +41,12 @@ static inline void push_table_to_stack(lua_State *L, int x, int y, int num) {
     lua_rawseti(L, -2, num);
 }
 
-static int insert_mid_jump_point(lua_State *L, struct map *m, int cur,
-            int father, int w, int num) {
+static int insert_mid_jump_point(lua_State* L,
+                                 struct map* m,
+                                 int cur,
+                                 int father,
+                                 int w,
+                                 int num) {
     int dx = cur % w - father % w;
     int dy = cur / w - father / w;
     if (dx == 0 || dy == 0) {
@@ -86,17 +87,22 @@ static int insert_mid_jump_point(lua_State *L, struct map *m, int cur,
     return 1;
 }
 
-static void flood_mark(struct map *m, int *visited, int pos, int connected_num, int limit) {
+static void flood_mark(struct map* m,
+                       int* visited,
+                       int pos,
+                       int connected_num,
+                       int limit) {
     if (visited[pos]) {
         return;
     }
     visited[pos] = 1;
     m->connected[pos] = connected_num;
-#define FLOOD(n) do { \
-    if (check_in_map_pos(n, limit) && !BITTEST(m->m, n)) { \
-        flood_mark(m, visited, n, connected_num, limit); \
-    } \
-} while(0);
+#define FLOOD(n)                                               \
+    do {                                                       \
+        if (check_in_map_pos(n, limit) && !BITTEST(m->m, n)) { \
+            flood_mark(m, visited, n, connected_num, limit);   \
+        }                                                      \
+    } while (0);
     FLOOD(pos - 1);
     FLOOD(pos + 1);
     FLOOD(pos - m->width);
@@ -104,9 +110,8 @@ static void flood_mark(struct map *m, int *visited, int pos, int connected_num, 
 #undef FLOOD
 }
 
-static int
-lnav_add_block(lua_State *L) {
-    struct map *m = luaL_checkudata(L, 1, MT_NAME);
+static int lnav_add_block(lua_State* L) {
+    struct map* m = luaL_checkudata(L, 1, MT_NAME);
     int x = luaL_checkinteger(L, 2);
     int y = luaL_checkinteger(L, 3);
     if (!check_in_map(x, y, m->width, m->height)) {
@@ -116,9 +121,8 @@ lnav_add_block(lua_State *L) {
     return 0;
 }
 
-static int
-lnav_blockset(lua_State *L) {
-    struct map *m = luaL_checkudata(L, 1, MT_NAME);
+static int lnav_blockset(lua_State* L) {
+    struct map* m = luaL_checkudata(L, 1, MT_NAME);
     luaL_checktype(L, 2, LUA_TTABLE);
     lua_settop(L, 2);
     int i = 1;
@@ -134,9 +138,8 @@ lnav_blockset(lua_State *L) {
     return 0;
 }
 
-static int
-lnav_clear_block(lua_State *L) {
-    struct map *m = luaL_checkudata(L, 1, MT_NAME);
+static int lnav_clear_block(lua_State* L) {
+    struct map* m = luaL_checkudata(L, 1, MT_NAME);
     int x = luaL_checkinteger(L, 2);
     int y = luaL_checkinteger(L, 3);
     if (!check_in_map(x, y, m->width, m->height)) {
@@ -146,9 +149,8 @@ lnav_clear_block(lua_State *L) {
     return 0;
 }
 
-static int
-lnav_clear_allblock(lua_State *L) {
-    struct map *m = luaL_checkudata(L, 1, MT_NAME);
+static int lnav_clear_allblock(lua_State* L) {
+    struct map* m = luaL_checkudata(L, 1, MT_NAME);
     int i;
     for (i = 0; i < m->width * m->height; i++) {
         BITCLEAR(m->m, i);
@@ -156,9 +158,8 @@ lnav_clear_allblock(lua_State *L) {
     return 0;
 }
 
-static int
-lnav_set_start(lua_State *L) {
-    struct map *m = luaL_checkudata(L, 1, MT_NAME);
+static int lnav_set_start(lua_State* L) {
+    struct map* m = luaL_checkudata(L, 1, MT_NAME);
     int x = luaL_checkinteger(L, 2);
     int y = luaL_checkinteger(L, 3);
     if (!check_in_map(x, y, m->width, m->height)) {
@@ -172,9 +173,8 @@ lnav_set_start(lua_State *L) {
     return 0;
 }
 
-static int
-lnav_set_end(lua_State *L) {
-    struct map *m = luaL_checkudata(L, 1, MT_NAME);
+static int lnav_set_end(lua_State* L) {
+    struct map* m = luaL_checkudata(L, 1, MT_NAME);
     int x = luaL_checkinteger(L, 2);
     int y = luaL_checkinteger(L, 3);
     if (!check_in_map(x, y, m->width, m->height)) {
@@ -188,11 +188,11 @@ lnav_set_end(lua_State *L) {
     return 0;
 }
 
-static int lnav_mark_connected(lua_State *L) {
-    struct map *m = luaL_checkudata(L, 1, MT_NAME);
+static int lnav_mark_connected(lua_State* L) {
+    struct map* m = luaL_checkudata(L, 1, MT_NAME);
     int len = m->width * m->height;
     if (!m->mark_connected) {
-        m->connected = (int *)malloc(len * sizeof(int));
+        m->connected = (int*)malloc(len * sizeof(int));
         m->mark_connected = 1;
     }
     memset(m->connected, 0, len * sizeof(int));
@@ -209,9 +209,8 @@ static int lnav_mark_connected(lua_State *L) {
     return 0;
 }
 
-static int
-lnav_dump_connected(lua_State *L) {
-    struct map *m = luaL_checkudata(L, 1, MT_NAME);
+static int lnav_dump_connected(lua_State* L) {
+    struct map* m = luaL_checkudata(L, 1, MT_NAME);
     printf("dump map connected state!!!!!!\n");
     if (!m->mark_connected) {
         printf("have not mark connected.\n");
@@ -232,9 +231,8 @@ lnav_dump_connected(lua_State *L) {
     return 0;
 }
 
-static int
-lnav_dump(lua_State *L) {
-    struct map *m = luaL_checkudata(L, 1, MT_NAME);
+static int lnav_dump(lua_State* L) {
+    struct map* m = luaL_checkudata(L, 1, MT_NAME);
     printf("dump map state!!!!!!\n");
     int i, pos;
 #ifdef __RECORD_PATH__
@@ -279,9 +277,8 @@ lnav_dump(lua_State *L) {
     return 0;
 }
 
-static int
-gc(lua_State *L) {
-    struct map *m = luaL_checkudata(L, 1, MT_NAME);
+static int gc(lua_State* L) {
+    struct map* m = luaL_checkudata(L, 1, MT_NAME);
     free(m->comefrom);
     free(m->open_set_map);
     if (m->mark_connected) {
@@ -290,13 +287,16 @@ gc(lua_State *L) {
     return 0;
 }
 
-static int
-form_path(lua_State *L, int last, struct map *m) {
-    int pos = last;
+static int form_path(lua_State* L, int last, struct map* m) {
     lua_newtable(L);
     int num = 0;
     int x, y;
     int w = m->width;
+    int pos = m->start;
+    x = pos % w;
+    y = pos / w;
+    push_table_to_stack(L, x, y, ++num);
+    pos = last;
 #ifdef __RECORD_PATH__
     int len = m->width * m->height;
 #endif
@@ -313,38 +313,36 @@ form_path(lua_State *L, int last, struct map *m) {
     return 1;
 }
 
-static int
-lnav_find_path(lua_State *L) {
-    struct map *m = luaL_checkudata(L, 1, MT_NAME);
+static int lnav_find_path(lua_State* L) {
+    struct map* m = luaL_checkudata(L, 1, MT_NAME);
     if (BITTEST(m->m, m->start)) {
-        luaL_error(L, "start pos(%d,%d) is in block", m->start % m->width, m->start / m->width);
+        luaL_error(L, "start pos(%d,%d) is in block", m->start % m->width,
+                   m->start / m->width);
     }
     if (BITTEST(m->m, m->end)) {
-        luaL_error(L, "end pos(%d,%d) is in block", m->end % m->width, m->end / m->width);
+        luaL_error(L, "end pos(%d,%d) is in block", m->end % m->width,
+                   m->end / m->width);
     }
     int start_pos = jps_find_path(m);
-    if(start_pos >= 0) {
+    if (start_pos >= 0) {
         return form_path(L, start_pos, m);
     }
     return 0;
 }
 
-static int
-lmetatable(lua_State *L) {
+static int lmetatable(lua_State* L) {
     if (luaL_newmetatable(L, MT_NAME)) {
-        luaL_Reg l[] = {
-            {"add_block", lnav_add_block},
-            {"add_blockset", lnav_blockset},
-            {"clear_block", lnav_clear_block},
-            {"clear_allblock", lnav_clear_allblock},
-            {"set_start", lnav_set_start},
-            {"set_end", lnav_set_end},
-            {"find_path", lnav_find_path},
-            {"mark_connected", lnav_mark_connected},
-            {"dump_connected", lnav_dump_connected},
-            {"dump", lnav_dump},
-            { NULL, NULL }
-        };
+        luaL_Reg l[] = {{"add_block", lnav_add_block},
+                        {"add_blockset", lnav_blockset},
+                        {"clear_block", lnav_clear_block},
+                        {"clear_allblock", lnav_clear_allblock},
+                        {"set_start", lnav_set_start},
+                        {"set_end", lnav_set_end},
+                        {"find_path", lnav_find_path},
+                        {"mark_connected", lnav_mark_connected},
+                        {"dump_connected", lnav_dump_connected},
+                        {"dump", lnav_dump},
+                        {NULL, NULL}};
         luaL_newlib(L, l);
         lua_setfield(L, -2, "__index");
 
@@ -354,8 +352,7 @@ lmetatable(lua_State *L) {
     return 1;
 }
 
-static int
-lnewmap(lua_State *L) {
+static int lnewmap(lua_State* L) {
     luaL_checktype(L, 1, LUA_TTABLE);
     lua_settop(L, 1);
     int width = getfield(L, "w");
@@ -367,14 +364,16 @@ lnewmap(lua_State *L) {
 #else
     int map_men_len = (BITSLOT(len) + 1) * 2;
 #endif
-    struct map *m = lua_newuserdata(L, sizeof(struct map) + map_men_len * sizeof(m->m[0]));
+    struct map* m =
+        lua_newuserdata(L, sizeof(struct map) + map_men_len * sizeof(m->m[0]));
     m->width = width;
     m->height = height;
     m->start = -1;
     m->end = -1;
     m->mark_connected = 0;
-    m->comefrom = (int *)malloc(len * sizeof(int));
-    m->open_set_map = (struct heap_node **)malloc(len * sizeof(struct heap_node *));
+    m->comefrom = (int*)malloc(len * sizeof(int));
+    m->open_set_map =
+        (struct heap_node**)malloc(len * sizeof(struct heap_node*));
     memset(m->m, 0, map_men_len * sizeof(m->m[0]));
     if (lua_getfield(L, 1, "obstacle") == LUA_TTABLE) {
         int i = 1;
@@ -395,12 +394,11 @@ lnewmap(lua_State *L) {
     return 1;
 }
 
-LUAMOD_API int
-luaopen_navigation(lua_State *L) {
+LUAMOD_API int luaopen_navigation(lua_State* L) {
     luaL_checkversion(L);
     luaL_Reg l[] = {
-        { "new", lnewmap },
-        { NULL, NULL },
+        {"new", lnewmap},
+        {NULL, NULL},
     };
     luaL_newlib(L, l);
     return 1;
