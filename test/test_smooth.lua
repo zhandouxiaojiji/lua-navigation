084822b1c576949c9c47857bad4d4d111232ddd8
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2022-06-02 19:02:18 +0800

    update README.md

--- test/test_smooth.lua
+++ test/test_smooth.lua
@@ -8,22 +8,6 @@ local nav = navigation.new {
     }
 }
 
-local function test_line_walkable(x1, y1, x2, y2)
-    local ret = nav:check_line_walkable(x1, y1, x2, y2)
-    print(string.format("check_line_walkable (%s, %s) => (%s, %s), %s", x1, y1, x2, y2, ret))
-    return ret
-end
-
-local function test_find_path_by_grid(x1, y1, x2, y2)
-    print("========================")
-    print(string.format("find path by grid (%s, %s) => (%s, %s)", x1, y1, x2, y2))
-    local ret = nav:find_path_by_grid(x1, y1, x2, y2)
-    for _, v in ipairs(ret) do
-        print(v[1], v[2])
-    end
-    print("========================")
-end
-
 local function test_find_path(x1, y1, x2, y2)
     print("========================")
     print(string.format("find path (%s, %s) => (%s, %s)", x1, y1, x2, y2))
