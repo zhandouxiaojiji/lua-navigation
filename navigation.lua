cf9c65a1f86f362319128c3959bad9ac5ce44a0f
zhandouxiaojiji <zhandouxiaojiji@gmail.com>
2022-10-18 14:20:17 +0800

    add navigation.lua

---
+++ navigation.lua
@@ -0,0 +1,153 @@
+local navigation_c = require "navigation.c"
+
+local mfloor = math.floor
+
+local mt = {}
+mt.__index = mt
+
+local function pos2cell(self, pos)
+    local x = mfloor(pos.x)
+    local y = mfloor(pos.y)
+    return y * self.w + x
+end
+
+local function cell2pos(self, cell)
+    return {
+        x = cell % self.w + 0.5,
+        y = cell // self.w
+    }
+end
+
+local function create_area(area_id)
+    return {
+        area_id = area_id,
+        paths = {}, -- {[from][to] -> path}
+        joints = {}, -- {cell -> true}
+    }
+end
+
+local function area_add_joint(self, area, pos)
+    local cell = pos2cell(self, pos)
+    area.joints[cell] = true
+    for from in pairs(area.joints) do
+        for to in pairs(area.joints) do
+            if not area.path[from][to] then
+                area.path[from][to] = self:find_path(cell2pos(from), cell2pos(to))
+            end
+        end
+    end
+end
+
+function mt:init(w, h, obstacles)
+    self.w = w
+    self.h = h
+    self.core = navigation_c.new {
+        w = w,
+        h = h,
+        obstacle = obstacles,
+    }
+    self.portals = {}
+    self.areas = {}
+    self.graph = {}
+end
+
+function mt:set_obstacle(pos)
+    self.core:add_block(mfloor(pos.x), mfloor(pos.y))
+end
+
+function mt:unset_obstacle(pos)
+    self.core:clear_block(mfloor(pos.x), mfloor(pos.y))
+end
+
+function mt:get_area_id_by_pos(pos)
+    return self.core:get_connected_id(mfloor(pos.x), mfloor(pos.y))
+end
+
+function mt:get_area(area_id)
+    local area = self.areas[area_id]
+    if not area then
+        area = create_area(area_id)
+        self.areas[area_id] = area
+    end
+    return area
+end
+
+function mt:add_portal(center_pos, camp, max_size)
+    local cell = pos2cell(self, center_pos)
+    local portal = {
+        cell = cell,
+        camp = camp,
+        joints = {}
+    }
+    self.portals[cell] = portal
+    local cx = mfloor(center_pos.x)
+    local cy = mfloor(center_pos.y)
+    max_size = max_size or 10
+    for i = 1, max_size // 2 do
+        if not self.core:is_block(cx - i, cy) and not self.core:is_block(cx + i, cy) then
+            portal.joints = {
+                {x = cx - i + 0.5, y = cy + 0.5},
+                {x = cx + i + 0.5, y = cy + 0.5},
+            }
+            break
+        end
+        if not self.core:is_block(cx, cy - i) and not self.core:is_block(cx, cy + i) then
+            portal.joints = {
+                {x = cx + 0.5, y = cy - i + 0.5},
+                {x = cx + 0.5, y = cy + i + 0.5},
+            }
+            break
+        end
+    end
+    for _, pos in pairs(portal.joints) do
+        local area_id = self:get_area_id_by_pos(pos)
+        local area = self:get_area(area_id)
+        area_add_joint(self, area, pos)
+    end
+end
+
+function mt:del_portal(pos)
+    local cell = pos2cell(self, pos)
+    print("todo del_portal", cell)
+    -- TODO
+end
+
+function mt:find_path(from, to, check_portal_func, ignore_list)
+    ignore_list = ignore_list or {}
+    if self.core:is_block(mfloor(from.x), mfloor(from.y)) then
+        ignore_list[#ignore_list+1] = from -- 自动忽略起点
+    end
+    for _, pos in pairs(ignore_list) do
+        self.core:clear_block(mfloor(pos.x), mfloor(pos.y))
+    end
+    local path
+    local ok, errmsg = xpcall(function()
+        path = self.core:find_path(from.x, from.y, to.x, to.y) or {}
+    end, debug.traceback)
+    if not ok then
+        print(errmsg)
+    end
+    for _, pos in pairs(ignore_list) do
+        self.core:add_block(mfloor(pos.x), mfloor(pos.y))
+    end
+    if #path < 2 then
+        print(string.format("cannot find path (%s, %s) =>(%s, %s)", from.x, from.y, to.x, to.y))
+    end
+    local new = {}
+    for _, pos in ipairs(path) do
+        new[#new+1] = {
+            x = pos[1],
+            y = pos[2]
+        }
+    end
+    return new
+end
+
+local M = {}
+function M.new(w, h, obstacles)
+    local obj = setmetatable({}, mt)
+    obj:init(w, h, obstacles)
+    return obj
+end
+
+return M
--- test/test.lua
+++ test/test.lua
@@ -1,44 +1,18 @@
-local navigation = require "navigation.c"
-local nav = navigation.new {
-    w = 20,
-    h = 20,
-    obstacle = {
-        -- {1, 0},
-    }
-}
+local navigation = require "navigation"
+local nav = navigation.new (20, 20, {
+    {1, 0},
+})
 
-local function test_line_walkable(x1, y1, x2, y2)
-    local ret = nav:check_line_walkable(x1, y1, x2, y2)
-    print(string.format("check_line_walkable (%s, %s) => (%s, %s), %s", x1, y1, x2, y2, ret))
-    return ret
-end
-
-local function test_find_path_by_grid(x1, y1, x2, y2)
+local function test_find_path(pos1, pos2)
     print("========================")
-    print(string.format("find path by grid (%s, %s) => (%s, %s)", x1, y1, x2, y2))
-    local ret = nav:find_path_by_grid(x1, y1, x2, y2)
+    print(string.format("find path (%s, %s) => (%s, %s)", pos1.x, pos1.y, pos2.x, pos2.y))
+    local ret = nav:find_path(pos1, pos2)
     for _, v in ipairs(ret) do
         print(v[1], v[2])
     end
     print("========================")
 end
 
-local function test_find_path(x1, y1, x2, y2)
-    print("========================")
-    print(string.format("find path (%s, %s) => (%s, %s)", x1, y1, x2, y2))
-    local ret = nav:find_path(x1, y1, x2, y2)
-    for _, v in ipairs(ret) do
-        print(v[1], v[2])
-    end
-    print("========================")
-end
-
--- test_line_walkable(0, 0, 1, 3)
--- test_line_walkable(0, 0, 3.5, 3)
-
--- test_find_path(0, 0, 1, 3.3)
--- test_find_path_by_grid(0, 0, 1, 3)
-
-test_find_path(1, 1, 0, 0)
-test_find_path(0, 0, 1, 1)
-test_find_path(1, 1, 0, 0)
+test_find_path({x = 1, y = 1}, {x = 0, y = 0})
+test_find_path({x = 0, y = 0}, {x = 1, y = 1})
+test_find_path({x = 1, y = 1}, {x = 0, y = 0})
